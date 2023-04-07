fde6323429bf4331b10d836d8644e0eecd1455e1
Константин Пигулевский <pigulevskii2000@gmail.com>
2023-04-07 20:40:41 +0300

    Reworked

--- main.py
+++ main.py
@@ -17,14 +17,16 @@ def count_average(grades):
 
 
 class Student:
+    student_list = []
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
         self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
-        self.grades = {}
+        self.grades = []
         self.ever_grade = []
+        self.student_list.append(self)
 
 
     def rate_hw(self, lecturer, course, grade):
@@ -49,6 +51,12 @@ class Student:
             print('Ошибка')
             return
         return self.ever_grade < other.ever_grade
+
+    def calc_ever(self):
+        self.grades = sum(self.grades)/len(self.grades)
+        #print(self.grades)
+        return self.grades
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -57,13 +65,17 @@ class Mentor:
 
 
 class Lecturer(Mentor):
+    lecturer_list = []
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.grades = {}
+        self.grades = []
         self.ever_grade = []
+        self.lecturer_list.append(self)
+
+
 
     def __str__(self):
-        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: '
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.ever_grade}'
         return res
 
     def __lt__(self, other):
@@ -71,6 +83,11 @@ class Lecturer(Mentor):
             print('Ошибка')
             return
         return self.ever_grade < other.ever_grade
+
+    def calc_ever(self):
+        self.grades = sum(self.grades)/len(self.grades)
+        #print(self.grades)
+        return self.grades
 
 
 
@@ -111,12 +128,22 @@ student_1.courses_in_progress += ['Git']
 student_1.courses_in_progress += ['ООП и работа с API']
 student_1.finished_courses += ['Вводный модуль']
 student_1.finished_courses += ['Основы языка программирования']
+student_1.grades += [8, 9, 7]
+student_1.grades += [10, 8, 9]
+student_1.ever_grade = student_1.calc_ever()
+
+#print(student_1.ever_grade)
 
 student_2 = Student('Анастасия', 'Сорша', 'Ж.')
 student_2.courses_in_progress += ['Git']
 student_2.courses_in_progress += ['ООП и работа с API']
 student_2.finished_courses += ['Вводный модуль']
 student_2.finished_courses += ['Основы языка программирования']
+student_2.grades += [6, 10, 7]
+student_2.grades += [9, 6, 8]
+student_2.ever_grade = student_2.calc_ever()
+
+#print(student_2.ever_grade)
 
 reviewer_1 = Reviewer('Евгений', 'Андропов')
 reviewer_1.courses_attached += ['Git']
@@ -129,32 +156,49 @@ reviewer_2.courses_attached += ['ООП и работа с API']
 lecturer_1 = Lecturer('Максим', 'Суржевский')
 lecturer_1.courses_attached += ['Git']
 lecturer_1.courses_attached += ['ООП и работа с API']
+lecturer_1.grades += [9, 10, 7]
+lecturer_1.grades += [8, 10, 10]
+lecturer_1.ever_grade = lecturer_1.calc_ever()
+
+#print((lecturer_1.ever_grade))
 
 lecturer_2 = Lecturer('Павел', 'Катаров')
 lecturer_2.courses_attached += ['Git']
 lecturer_2.courses_attached += ['ООП и работа с API']
+lecturer_2.grades += [9, 10, 10]
+lecturer_2.grades += [8, 10, 8]
+lecturer_2.ever_grade = lecturer_2.calc_ever()
 
-reviewer_1.rate_hw(student_1, 'Python', 9)
-reviewer_1.rate_hw(student_2, 'Python', 5)
-reviewer_1.rate_hw(student_1, 'VB', 9)
+#print((lecturer_2.ever_grade))
 
-reviewer_2.rate_hw(student_1, 'Python', 9)
-reviewer_2.rate_hw(student_2, 'Python', 3)
-reviewer_2.rate_hw(student_2, 'C++', 10)
+stud_grade_list = [student_1.grades, student_2.grades]
 
-student_1.rate_hw(lecturer_1, 'Python', 9)
-student_1.rate_hw(lecturer_1, 'VB', 9)
-student_1.rate_hw(lecturer_1, 'Python', 8)
+#ever_grade1 = sum(student_1.grades)/len(student_1.grades)
+#ever_grade2 = sum(student_2.grades)/len(student_2.grades)
 
-student_2.rate_hw(lecturer_2, 'Python', 8)
-student_2.rate_hw(lecturer_2, 'VB', 8)
-student_2.rate_hw(lecturer_2, 'Python', 9)
+
+#student_1.ever_grade = ever_grade1
+#student_2.ever_grade = ever_grade2
+
+#ever_grade3 = sum(lecturer_1.grades)/len(lecturer_1.grades)
+#ever_grade4 = sum(lecturer_2.grades)/len(lecturer_2.grades)
+
+#lecturer_1.ever_grade = ever_grade3
+#lecturer_2.ever_grade = ever_grade4
+
+#student_list = [student_1, student_2]
+
+#for i in student_list:
+    #if int in i:
+        #print('t')
+
 
 print(student_1)
 print(reviewer_1)
 print(lecturer_1)
 print(student_1 > student_2)
 print(lecturer_1 > lecturer_2)
+
 
 
 
